15b808e39b1ad8f3665ac05be22b2f87e4b60ee5
Erkki Heino <erkki.heino@aalto.fi>
2017-08-15 15:06:36 +0300

    Handle year-only birth and death dates

--- src/scripts/filters.js
+++ src/scripts/filters.js
@@ -3,10 +3,25 @@
     'use strict';
 
     angular.module('facetApp')
-    .filter('castArray', function(_) {
+
+    .filter('castArray', castArray)
+    .filter('dateOrYear', dateOrYear);
+
+    /* @ngInject */
+    function castArray(_) {
         return function(input) {
             return _.castArray(input);
         };
-    });
+    }
+
+    /* @ngInject */
+    function dateOrYear($filter, dateFilter) {
+        return function(input, format, timezone) {
+            if (input.toString().match(/^\d{4}$/)) {
+                return input;
+            }
+            return dateFilter(input, format, timezone);
+        };
+    }
 
 })();
